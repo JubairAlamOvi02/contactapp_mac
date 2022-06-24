ca716e0c7e90f45f5364393d17960887419b96c9
Jubair Alam Ovi <61586952+JubairAlamOvi02@users.noreply.github.com>
2022-06-24 13:03:17 +0600

    pages

---
+++ lib/pages/contact_list_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ContactListPage extends StatefulWidget {
+  static const String routeName = '/contact_list';
+
+  @override
+  State<ContactListPage> createState() => _ContactListPageState();
+}
+
+class _ContactListPageState extends State<ContactListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
